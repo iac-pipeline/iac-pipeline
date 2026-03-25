d2d56cac74f353d47e83edfcaa7917502cf75722
JamesB2204 <jamescbradford22@gmail.com>
2026-03-25 17:28:34 +0000

    adding in code to comment on pr

---
+++ .github/actions/write_comment/write_comment.sh
@@ -0,0 +1,8 @@
+set -e
+echo $body_to_post
+curl -L -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$client_repository/issues/$client_pull_request_number/comments \
+  -d "$(jq -n --arg body "$body_to_post" '{"body": $body}')"
