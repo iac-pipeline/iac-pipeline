e5a6af2eebba8c94d94d5fa74d38c641f981317f
JamesB2204 <jamescbradford22@gmail.com>
2026-05-07 20:48:57 +0100

    testing checkov custom policy

--- .github/actions/write_comment/write_comment.sh
+++ .github/actions/write_comment/write_comment.sh
@@ -15,7 +15,7 @@ existing_comment_id=$(curl -s -L \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/repos/$client_repository/issues/$client_pull_request_number/comments" \
-  | jq -r --arg marker "<!-- ${comment_marker} -->" \
+  | jq -r --arg marker "$comment_marker" \
     '[.[] | select(.body | contains("<!-- " + $marker + " -->"))] | first | .id // empty')
 
 if [ -n "$existing_comment_id" ]; then
